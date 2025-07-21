025818e909f97db8c58511fccb947f39b5ef359c
maviharsh <maviharsh@gmail.com>
2025-07-21 20:32:41 +0530

    Changes made in cors policy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000", // Your React frontend URL
+    origin: "https://cricketscorecardapp-website.onrender.com", 
     credentials: true
   }));
 app.use(cookieParser());
